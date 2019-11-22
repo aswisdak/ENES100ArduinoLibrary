355918bc9584209c1a7f44da44181433cba0d592
nperkin3 <nperkin3@umd.edu>
2019-11-22 13:05:43 -0500

    Update BlackBox.ino

--- examples/BlackBox/BlackBox.ino
+++ examples/BlackBox/BlackBox.ino
@@ -1,28 +1,28 @@
 #include "Enes100.h"
 
 void setup() {
+    //Allow Wifi Modules Time to Start Up
+    delay(2000);
     // Initialize Enes100 library
     // Team Name, Mission Type, Marker ID, TX Pin, RX Pin
     Enes100.begin("Black Box Team", BLACK_BOX, 3, 8, 9);
 
+    // Transmit the color of the LED
+    Enes100.mission("Green");
     // Any other setup code...
 }
 
 void loop() {
     // Update the OSV's current location
-    if (Enes100.updateLocation()) {
-        Enes100.print("OSV is at (");
-        Enes100.print(Enes100.location.x);
-        Enes100.print(", ");
-        Enes100.print(Enes100.location.y);
-        Enes100.print(", ");
-        Enes100.print(Enes100.location.theta);
-        Enes100.println(")");
-    } else {
-        // OSV's location was not found
+    while(!Enes100.updateLocation()) {
         Enes100.println("404 Not Found");
     }
+    Enes100.print("OSV is at (");
+    Enes100.print(Enes100.location.x);
+    Enes100.print(", ");
+    Enes100.print(Enes100.location.y);
+    Enes100.print(", ");
+    Enes100.print(Enes100.location.theta);
+    Enes100.println(")");
 
-    // Transmit the color of the LED
-    Enes100.mission("Green");
 }
