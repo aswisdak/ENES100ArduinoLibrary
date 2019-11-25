d658801a2cc22ef8d8e7bceaf7c1ebafd4722eb6
nperkin3 <nperkin3@umd.edu>
2019-11-25 13:36:52 -0500

    Update fire.ino

--- examples/Fire/Fire.ino
+++ examples/Fire/Fire.ino
@@ -1,34 +1,32 @@
 #include "Enes100.h"
 
 void setup() {
-    // Initialize Enes100 library
-    // Team Name, Mission Type, Marker ID, TX Pin, RX Pin
+    //Allow Wifi Modules Time to Start Up
+    delay(2000);
+    // Initialize Enes100 library (Team Name, Mission Type, Marker ID, TX Pin, RX Pin)
     Enes100.begin("It's Lit", FIRE, 3, 8, 9);
-
+    //Print out destination coordinates
     Enes100.print("Destination is at (");
     Enes100.print(Enes100.destination.x);
     Enes100.print(", ");
     Enes100.print(Enes100.destination.y);
     Enes100.println(")");
-
+    // Transmit the number of candles that are lit
+    Enes100.mission(4);
     // Any other setup code...
 }
 
 void loop() {
     // Update the OSV's current location
-    if (Enes100.updateLocation()) {
-        Enes100.print("OSV is at (");
-        Enes100.print(Enes100.location.x);
-        Enes100.print(", ");
-        Enes100.print(Enes100.location.y);
-        Enes100.print(", ");
-        Enes100.print(Enes100.location.theta);
-        Enes100.println(")");
-    } else {
-        // OSV's location was not found
+    while(!Enes100.updateLocation()) {
         Enes100.println("404 Not Found");
     }
-    
-    // Transmit the number of candles that are lit
-    Enes100.mission(4);
+    //Once updateLocation() is successfull, print out location coordinates
+    Enes100.print("OSV is at (");
+    Enes100.print(Enes100.location.x);
+    Enes100.print(", ");
+    Enes100.print(Enes100.location.y);
+    Enes100.print(", ");
+    Enes100.print(Enes100.location.theta);
+    Enes100.println(")");
 }
