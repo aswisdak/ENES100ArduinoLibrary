a49d9efa08dc91f54e48f0c1b26f1525729e07f0
nperkin3 <nperkin3@umd.edu>
2019-11-25 13:39:11 -0500

    Update water.ino

--- examples/Water/Water.ino
+++ examples/Water/Water.ino
@@ -1,37 +1,34 @@
 #include "Enes100.h"
 
 void setup() {
-    // Initialize Enes100 Library
-    // Team Name, Mission Type, Marker ID, TX Pin, RX Pin
+    //Allow Wifi Modules Time to Start Up
+    delay(2000);
+    // Initialize Enes100 library (Team Name, Mission Type, Marker ID, TX Pin, RX Pin)
     Enes100.begin("Water We Doing", WATER, 3, 8, 9);
-
+    //Print out destination coordinates
     Enes100.print("Destination is at (");
     Enes100.print(Enes100.destination.x);
     Enes100.print(", ");
     Enes100.print(Enes100.destination.y);
     Enes100.println(")");
-
+    // Transmit the state of the pool
+    Enes100.mission(FRESH_POLLUTED);
+    // Transmit the depth of the pool in mm
+    Enes100.mission(44.2);
     // Any other setup code...
 }
 
 void loop() {
     // Update the OSV's current location
-    if (Enes100.updateLocation()) {
-        Enes100.print("OSV is at (");
-        Enes100.print(Enes100.location.x);
-        Enes100.print(", ");
-        Enes100.print(Enes100.location.y);
-        Enes100.print(", ");
-        Enes100.print(Enes100.location.theta);
-        Enes100.println(")");
-    } else {
-        // OSV's location was not found
+    while(!Enes100.updateLocation()) {
         Enes100.println("404 Not Found");
     }
-
-    // Transmit the state of the pool
-    Enes100.mission(FRESH_POLLUTED);
-
-    // Transmit the depth of the pool in mm
-    Enes100.mission(44.2);
+    //Once updateLocation() is successfull, print out location coordinates
+    Enes100.print("OSV is at (");
+    Enes100.print(Enes100.location.x);
+    Enes100.print(", ");
+    Enes100.print(Enes100.location.y);
+    Enes100.print(", ");
+    Enes100.print(Enes100.location.theta);
+    Enes100.println(")");
 }
