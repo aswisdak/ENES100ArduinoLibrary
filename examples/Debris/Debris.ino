dcf7783840368df144fd84227e5b092be082997c
nperkin3 <nperkin3@umd.edu>
2019-11-25 13:34:06 -0500

    Update debris.ino

--- examples/Debris/Debris.ino
+++ examples/Debris/Debris.ino
@@ -1,38 +1,34 @@
 #include "Enes100.h"
 
 void setup() {
-    // Initialize Enes100 library
-    // Team Name, Mission Type, Marker ID, TX Pin, RX Pin
+    //Allow Wifi Modules Time to Start Up
+    delay(2000);
+    // Initialize Enes100 library (Team Name, Mission Type, Marker ID, TX Pin, RX Pin)
     Enes100.begin("Duhbree", DEBRIS, 3, 8, 9);
-
-    //Print Destination Coordinates
+    //Print out destination coordinates
     Enes100.print("Destination is at (");
     Enes100.print(Enes100.destination.x);
     Enes100.print(", ");
     Enes100.print(Enes100.destination.y);
     Enes100.println(")");
-
+    // Transmit the material of the debris (Plastic or Copper)
+    Enes100.mission(Plastic);
+    // Transmit the mass of the debris in grams
+    Enes100.mission(2.43);
     // Any other setup code...
 }
 
 void loop() {
     // Update the OSV's current location
-    if (Enes100.updateLocation()) {
-        Enes100.print("OSV is at (");
-        Enes100.print(Enes100.location.x);
-        Enes100.print(", ");
-        Enes100.print(Enes100.location.y);
-        Enes100.print(", ");
-        Enes100.print(Enes100.location.theta);
-        Enes100.println(")");
-    } else {
-        // OSV's location was not found
+    while(!Enes100.updateLocation()) {
         Enes100.println("404 Not Found");
     }
-
-    // Transmit the material of the debris (Plastic or Copper)
-    Enes100.mission(Plastic);
-
-    // Transmit the mass of the debris in grams
-    Enes100.mission(2.43);
+    //Once updateLocation() is successfull, print out location coordinates
+    Enes100.print("OSV is at (");
+    Enes100.print(Enes100.location.x);
+    Enes100.print(", ");
+    Enes100.print(Enes100.location.y);
+    Enes100.print(", ");
+    Enes100.print(Enes100.location.theta);
+    Enes100.println(")");
 }
